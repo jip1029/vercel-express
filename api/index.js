168d7ea2afc2dc232e636c360c4cb20ef02d5864
Josh <you@example.com>
2023-04-05 08:33:28 -0400

    added db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,63 @@
 const app = require('express')();
 const { v4 } = require('uuid');
+const mongoose = require('mongoose');
+const request = require('request');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+const secret = crypto.randomBytes(32).toString('hex');
+
+app.use(
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(
+  'mongodb+srv://jip1029:Updown1029@nasaimageappdb.h4indjy.mongodb.net/?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: 'public' });
+});
+
+app.post('/', async (req, res) => {
+  const saltRounds = 10;
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  const newUser = new User({
+    username,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.redirect('/login');
+  } catch (err) {
+    console.log(err);
+    res.send('An error occurred, please try again.');
+  }
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile('login.html', { root: 'public' });
 });
 
 app.get('/api', (req, res) => {
